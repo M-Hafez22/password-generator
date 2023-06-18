23ffc206f3d184c765d99aa80acc9a1918c5c310
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-06-18 23:15:30 +0300

    Create Password

--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
 // Password length
 const pwLengthOutputEl = document.getElementById("length")!
 const pwLengthSliderEl = document.getElementById("pw-slider")!
+// output
+const pwEl = document.getElementById("password")!
+const generatorEL = document.getElementById("generator")
+// Checkboxs
+const numberCheckboxEl = document.getElementById("number")
+const upperCheckboxEl = document.getElementById("upper")
+const lowerCheckboxEl = document.getElementById("lower")
+const symbolsCheckboxEl = document.getElementById("symbols")
+const ambiguousSymbolsCheckboxEl = document.getElementById("ambiguousSymbols")
+const rateEl = document.getElementById("rate")!
 
 // Get password length
 let pwlength = 14 // The default password length
@@ -17,17 +27,62 @@ const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 const symbols = "!@#$%^&*_+="
 const ambiguousSymbols = "{}[]()/\\'\"`~,;:.<>"
+
 // Create Password
-// Select password characters from the password range
+const generatePW = (pwLenght: number): string => {
+  let password = ""
+  // Create Password Range
+  let passwordRange = ""
+  if ((numberCheckboxEl as HTMLInputElement).checked) {
+    passwordRange += shufflingString(numbers)
+  }
+  if ((upperCheckboxEl as HTMLInputElement).checked) {
+    passwordRange += shufflingString(upperLetters)
+  }
+  if ((lowerCheckboxEl as HTMLInputElement).checked) {
+    passwordRange += shufflingString(lowerLetters)
+  }
+  if ((symbolsCheckboxEl as HTMLInputElement).checked) {
+    passwordRange += shufflingString(symbols)
+  }
+  if ((ambiguousSymbolsCheckboxEl as HTMLInputElement).checked) {
+    passwordRange += shufflingString(ambiguousSymbols)
+  }
+  if (!passwordRange) {
+    console.log("Select password character types")
+    pwEl.textContent = "Select password character types"
+    return "Select password character types"
+  }
+  // Select password characters from the password range
+  for (let i = 0; i < pwLenght; i++) {
+    password +=
+      shufflingString(passwordRange)[
+        Math.floor(Math.random() * passwordRange.length)
+      ]
+  }
+  pwEl.textContent = password
+  // Rating the password (Bad, Weak, Strong)
+  if (password.length > 11) {
+    rateEl.textContent = "Good"
+    pwLengthSliderEl.style.backgroundColor = "green"
+  } else if (password.length > 5) {
+    rateEl.textContent = "Weak"
+    pwLengthSliderEl.style.backgroundColor = "blue"
+  } else if (password.length > 3) {
+    rateEl.textContent = "Bad"
+    pwLengthSliderEl.style.backgroundColor = "red"
+  }
+  return password
+}
 
-// Rating the password (Bad, Weak, Strong)
+// TODO Update password when setting changed
+// TODO Update password when generate button clicked
 
-// Update password when setting changed
-// Update password when generate button clicked
-// Create a password at the application startup
+// TODO Copy password to the clipboard
 
-// Copy password to the clipboard
 // shuffling a String
 const shufflingString = (str: string): string => {
   return [...str].sort(() => Math.random() - 0.5).join("")
 }
+// Create a password at the application startup
+generatePW(pwlength)
--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,16 @@
 // Password length
 const pwLengthOutputEl = document.getElementById("length");
 const pwLengthSliderEl = document.getElementById("pw-slider");
+// output
+const pwEl = document.getElementById("password");
+const generatorEL = document.getElementById("generator");
+// Checkboxs
+const numberCheckboxEl = document.getElementById("number");
+const upperCheckboxEl = document.getElementById("upper");
+const lowerCheckboxEl = document.getElementById("lower");
+const symbolsCheckboxEl = document.getElementById("symbols");
+const ambiguousSymbolsCheckboxEl = document.getElementById("ambiguousSymbols");
+const rateEl = document.getElementById("rate");
 // Get password length
 let pwlength = 14; // The default password length
 // Show the Password Length
@@ -17,9 +27,57 @@ const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const symbols = "!@#$%^&*_+=";
 const ambiguousSymbols = "{}[]()/\\'\"`~,;:.<>";
 // Create Password
-// Select password characters from the password range
-// Rating the password (Bad, Weak, Strong)
-// Update password when setting changed
-// Update password when generate button clicked
+const generatePW = (pwLenght) => {
+    let password = "";
+    // Create Password Range
+    let passwordRange = "";
+    if (numberCheckboxEl.checked) {
+        passwordRange += shufflingString(numbers);
+    }
+    if (upperCheckboxEl.checked) {
+        passwordRange += shufflingString(upperLetters);
+    }
+    if (lowerCheckboxEl.checked) {
+        passwordRange += shufflingString(lowerLetters);
+    }
+    if (symbolsCheckboxEl.checked) {
+        passwordRange += shufflingString(symbols);
+    }
+    if (ambiguousSymbolsCheckboxEl.checked) {
+        passwordRange += shufflingString(ambiguousSymbols);
+    }
+    if (!passwordRange) {
+        console.log("Select password character types");
+        pwEl.textContent = "Select password character types";
+        return "Select password character types";
+    }
+    // Select password characters from the password range
+    for (let i = 0; i < pwLenght; i++) {
+        password +=
+            shufflingString(passwordRange)[Math.floor(Math.random() * passwordRange.length)];
+    }
+    pwEl.textContent = password;
+    // Rating the password (Bad, Weak, Strong)
+    if (password.length > 11) {
+        rateEl.textContent = "Good";
+        pwLengthSliderEl.style.backgroundColor = "green";
+    }
+    else if (password.length > 5) {
+        rateEl.textContent = "Weak";
+        pwLengthSliderEl.style.backgroundColor = "blue";
+    }
+    else if (password.length > 3) {
+        rateEl.textContent = "Bad";
+        pwLengthSliderEl.style.backgroundColor = "red";
+    }
+    return password;
+};
+// TODO Update password when setting changed
+// TODO Update password when generate button clicked
+// TODO Copy password to the clipboard
+// shuffling a String
+const shufflingString = (str) => {
+    return [...str].sort(() => Math.random() - 0.5).join("");
+};
 // Create a password at the application startup
-// Copy password to the clipboard
+generatePW(pwlength);
