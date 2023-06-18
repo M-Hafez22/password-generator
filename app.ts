df5c2dd940437f6da0d2dc3b91008680af8001f6
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-06-18 23:02:47 +0300

    📝 App plan

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("Start")
+// Get password length
+
+// Create Password
+// Create password range
+// Select password characters from the password range
+
+// Rating the password (Bad, Weak, Strong)
+
+// Update password when setting changed
+// Update password when generate button clicked
+// Create a password at the application startup
+
+// Copy password to the clipboard
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Start");
+// Get password length
+// Create Password
+// Create password range
+// Select password characters from the password range
+// Rating the password (Bad, Weak, Strong)
+// Update password when setting changed
+// Update password when generate button clicked
+// Create a password at the application startup
+// Copy password to the clipboard
