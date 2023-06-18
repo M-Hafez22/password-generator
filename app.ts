96e4d1748a772b0e56f683048ba5e260d01729c8
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-06-18 23:05:40 +0300

    Get password length

--- app.ts
+++ app.ts
@@ -1,5 +1,15 @@
-// Get password length
+// Password length
+const pwLengthOutputEl = document.getElementById("length")!
+const pwLengthSliderEl = document.getElementById("pw-slider")!
 
+// Get password length
+let pwlength = 14 // The default password length
+// Show the Password Length
+pwLengthOutputEl.textContent = pwlength.toString()
+pwLengthSliderEl?.addEventListener("input", function () {
+  pwlength = parseInt((pwLengthSliderEl as HTMLInputElement).value)
+  pwLengthOutputEl.textContent = pwlength.toString()
+})
 // Create Password
 // Create password range
 // Select password characters from the password range
